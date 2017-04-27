df608906cb6de3863823559b6fdf0bab03de3951
Aidan Morgan <aidan.morgan@azimuthlabs.com.au>
2017-04-27 23:41:05 +0800

    Removing the deleteResourceGroup commands to remove any temptation to use them.

--- remove/lib/deleteResourceGroup.js
+++
@@ -1,9 +0,0 @@
-'use strict';
-
-module.exports = {
-  deleteResourceGroup () {
-    return this.provider.LoginWithServicePrincipal()
-      .then(() => this.provider.DeleteDeployment())
-      .then(() => this.provider.DeleteResourceGroup());
-  }
-};
